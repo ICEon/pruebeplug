a4cacfd509bbdd0a4f925dfb767d1004c7ac7fd9
ICEon <matrix_eon@yahoo.com>
2014-08-06 06:24:34 -0500

    mario3

--- js/acciones.js
+++ js/acciones.js
@@ -13,13 +13,13 @@ lla = window.plugins.LowLatencyAudio;
 lla.preloadFX('assets/bass.mp3', 'assets/bass.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg ); });
 lla.preloadFX('assets/snare.mp3', 'assets/snare.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg ); });
 lla.preloadFX('assets/highhat.mp3', 'assets/highhat.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg ); });
-lla.preloadFX('assets/mario.mp3', 'mario', function(msg){}, function(msg){ alert( 'Error: ' + msg ); });	
+lla.preloadFX('assets/mario.mp3', 'assets/mario.mp3', function(msg){}, function(msg){ alert( 'Error: ' + msg ); });	
 }
 }
 
 function play(drum) {
 document.getElementById(drum).className = 'drum touched';
-lla.play('mario');
+lla.play('assets/mario.mp3');
 }
 
 function touchEnd(event) {
